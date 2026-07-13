d79c6c4994f4aa2dc3a99160c1dda78f4c047720
ysz2912481870@gmail.com <ysz2912481870@gmail.com>
2026-07-13 10:23:46 +0800

    后端初始化

---
+++ camera-access-backend/src/main/java/com/heichan/camera_access_backend/CameraAccessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heichan.camera_access_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameraAccessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CameraAccessBackendApplication.class, args);
+	}
+
+}
---
+++ camera-access-backend/src/test/java/com/heichan/camera_access_backend/CameraAccessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heichan.camera_access_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CameraAccessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
